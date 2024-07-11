af630cf6daa6682b9f992683dadea881e567ec66
Svetlana Novikova <seaciliya2012@yandex.ru>
2024-07-11 22:24:34 +0300

    'HW24.3 1'

--- materials/model.py
+++ materials/model.py
@@ -45,3 +45,32 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = "урок"
         verbose_name_plural = "уроки"
+
+
+class Subscription(models.Model):
+    """ Модель подписки """
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь"
+    )
+    course = models.ForeignKey(
+        "materials.Course",
+        on_delete=models.CASCADE,
+        related_name="Курс",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def get_user_email(self):
+        return self.user.email
+
+    def __str__(self):
+        return f"{self.user.email} подписан на {self.course.name}."
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+        unique_together = ('user', 'course')
--- materials/serializers.py
+++ materials/serializers.py
@@ -2,9 +2,11 @@ from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.model import Course, Lesson
+from materials.validators import YoutubeLinkValidator
 
 
 class LessonSerializer(ModelSerializer):
+    validators = [YoutubeLinkValidator(field='link_to_video')]
     class Meta:
         model = Lesson
         fields = "__all__"
